a9d753c227ade77845c87545b5421d46076df598
hk-11 <71665544+hk-11@users.noreply.github.com>
2023-07-08 11:14:50 -0700

    Update types.js

--- React Chat/src/action/types.js
+++ React Chat/src/action/types.js
@@ -1,11 +1,9 @@
-// User Action Types
-//ye ap ke reducer ke action
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
 
 export const REGISTER_USER = "REGISTER_USER";
 
-//msg Types
+
 
 export const ADD_ITEM = "ADD_ITEM";
 export const DELETE_ITEM = "DELETE_ITEM";
